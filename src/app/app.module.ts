95c1752632157bd5384ef8fa10c1b75672e3bc07
Eric Martinez <emartinez@usgs.gov>
2017-11-27 10:57:29 -0700

    Initial geoserve places.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GeoserveComponent } from './geoserve/geoserve.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeoserveComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/geoserve/geoserve.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+const GEOSERVE_PLACES_ENDPOINT: string = 'https://earthquake.usgs.gov/ws/geoserve/places.json';
+
+@Component({
+  selector: 'app-geoserve',
+  templateUrl: './geoserve.component.html',
+  styleUrls: ['./geoserve.component.css']
+})
+export class GeoserveComponent implements OnInit {
+  places: object[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.handleUpdateLocationClick(34.0, -118.0);
+  }
+
+  handleUpdateLocationClick (latitude: string, longitude: string) {
+    fetch(`${GEOSERVE_PLACES_ENDPOINT}?latitude=${latitude}&longitude=${longitude}&type=event`)
+      .then((response) => {
+        return response.json();
+      }).then((json) => {
+        this.places = json.event.features;
+      });
+  }
+
+  formattedAzimuth (place) {
+    let fullwind = 22.5
+    let directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE',
+      'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N']
+    let azimuth = place.properties.azimuth
+
+    // if direction is already in compass points
+    if (directions.indexOf(azimuth) > -1) {
+      return azimuth
+    }
+
+    return directions[(Math.round((azimuth % 360) / fullwind))]
+  }
+
+  formattedDistance (place) {
+    return place.properties.distance + ' km'
+  }
+
+
+}
